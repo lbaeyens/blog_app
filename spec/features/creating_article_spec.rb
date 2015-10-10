36dfc483cbbdb65ede11a6155eb131a430dc0d68
Lieven Baeyens <isidoorbaeyens@gmail.com>
2015-10-10 13:02:19 +0200

    implement listing articles

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -7,6 +7,7 @@ RSpec.feature "Creating Articles" do
     fill_in "Title",  with: "Creating first article"
     fill_in "Body", with: "Lorem Ipsum"
     click_button "Create Article"
+
     expect(page).to have_content("Article has been created")
     expect(page.current_path).to eq(articles_path)
   end
@@ -17,6 +18,7 @@ RSpec.feature "Creating Articles" do
     fill_in "Title",  with: ""
     fill_in "Body", with: ""
     click_button "Create Article"
+
     expect(page).to have_content("Article has not been created")
     expect(page).to have_content("Title can't be blank")
     expect(page).to have_content("Body can't be blank")
