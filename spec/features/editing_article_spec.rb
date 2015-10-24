c2cc1f2f55e236a0b3fe94404f82682a132e63b4
Lieven Baeyens <isidoorbaeyens@gmail.com>
2015-10-24 22:08:16 +0200

    fix broken tests (caused by enforcing authentication)

--- spec/features/editing_article_spec.rb
+++ spec/features/editing_article_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.feature "Editing an Article" do
-    before do
-      @article = Article.create(title: "the first article", body: "body of first article")
-    end
+
+  before do
+    @john = User.create!(email: "john@example.com", password:"password")
+    visit "/"
+    click_link "Sign in"
+    fill_in "Email",  with: @john.email
+    fill_in "Password", with: @john.password
+    click_button "Log in"
+    @article = Article.create(title: "the first article", body: "body of first article")
+   end
 
   scenario "A user updates an article" do
     visit "/"
