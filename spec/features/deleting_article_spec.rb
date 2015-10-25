ce9cff731b3c9aa449ece9fc3a6ddcc79b98125f
Lieven Baeyens <isidoorbaeyens@gmail.com>
2015-10-25 10:31:54 +0100

    link articles to users

--- spec/features/deleting_article_spec.rb
+++ spec/features/deleting_article_spec.rb
@@ -3,11 +3,7 @@ require 'rails_helper'
 RSpec.feature "Deleting an Article" do
   before do
     @john = User.create!(email: "john@example.com", password:"password")
-    visit "/"
-    click_link "Sign in"
-    fill_in "Email",  with: @john.email
-    fill_in "Password", with: @john.password
-    click_button "Log in"
+    login_as(@john)
 
     @article = Article.create(title: "the first article", body: "body of first article")
   end
--- spec/features/editing_article_spec.rb
+++ spec/features/editing_article_spec.rb
@@ -4,11 +4,7 @@ RSpec.feature "Editing an Article" do
 
   before do
     @john = User.create!(email: "john@example.com", password:"password")
-    visit "/"
-    click_link "Sign in"
-    fill_in "Email",  with: @john.email
-    fill_in "Password", with: @john.password
-    click_button "Log in"
+    login_as(@john)
     @article = Article.create(title: "the first article", body: "body of first article")
    end
 
